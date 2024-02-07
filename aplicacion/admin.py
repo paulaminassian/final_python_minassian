e6adfc4dbba70bba2b707c4fadc68d1d41db2304
Paula Minassian <pau.m-v@hotmail.com>
2024-02-07 10:18:11 -0300

    actualice strings en models

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import *
-
+from django.contrib import admin
+from .models import Clientes, Maritimas, Rutas, Precios
 # Register your models here.
 
 admin.site.register(Clientes)
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -5,16 +5,28 @@ from django.db import models
 class Clientes(models.Model):
     razon_social = models.CharField(max_length=50)
     contacto = models.EmailField()
+    
+    def __str__(self):
+        return f'{self.razon_social}'
 
 class Maritimas(models.Model):
     maritima = models.CharField(max_length=50)
     contacto = models.EmailField()
     
+    def __str__(self):
+        return f'{self.maritima}'
+    
 class Rutas(models.Model):
     origen = models.CharField(max_length=50)
     destino = models.CharField(max_length=50) 
     
+    def __str__(self):
+        return f'{self.origen} - {self.destino}'
+
+    
 class Precios(models.Model):
     ruta = models.CharField(max_length = 80)
     precio=models.IntegerField()
-
+    
+    def __str__(self):
+        return f'{self.ruta}'
